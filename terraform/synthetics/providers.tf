9160145b0541082f17f9b5203dc150d3ebcc8a20
Jason Sustarich <j.sustarich@f5.com>
2026-05-14 11:32:29 +0100

    Fun times

---
+++ terraform/synthetics/providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    grafana = { source = "grafana/grafana" }
+  }
+}
+
+provider "grafana" {
+  url             = "https://mindlessmuppet.grafana.net/"
+  auth            = var.grafana_service_token
+  sm_url          = "https://synthetic-monitoring-api-gb-south-1.grafana.net"
+  sm_access_token = var.sm_access_token
+}
